2607125701c04e093243ff73baac05f5f5aec07d
Murray Colpman <muzer@tim32.org>
2016-09-24 16:15:36 +0100

    Correct sed and patch commands

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ this="$PWD"
 
 cd ~
 echo Copying pisstv script, replacing HOMEDIR with "$PWD"
-sed s/HOMEDIR/"$PWD"/g "$this"/scripts/pisstv.sh > pisstv.sh
+sed s@HOMEDIR@"$PWD"@g "$this"/scripts/pisstv.sh > pisstv.sh
 
 echo Setting pisstv script as executable
 chmod +x pisstv.sh
@@ -13,7 +13,7 @@ echo Extracting PiFM
 tar -xf "$this"/src/pifm.tar.gz
 
 echo Patching pifm
-patch -Np1 -i "$this"/src/pifm.patch
+patch -N -i "$this"/src/pifm.patch
 
 echo Compiling pifm
 gcc -o pifm pifm.c
@@ -28,7 +28,7 @@ read -r -p \
   'Do you want me to overwrite your /etc/rc.local with ours? If you have modified it, probably best not to... (y/N) ' overwrite
 if [ "$overwrite" = "y" ]; then
   echo Replacing rc.local, replacing HOMEDIR with "$PWD"
-  sed s/HOMEDIR/"$PWD"/g "$this"/config/rc.local | sudo tee /etc/rc.local
+  sed s@HOMEDIR@"$PWD"@g "$this"/config/rc.local | sudo tee /etc/rc.local
 else
   echo Please add \""$PWD"/pisstv.sh \&\" to your rc.local or equivalent
 fi
