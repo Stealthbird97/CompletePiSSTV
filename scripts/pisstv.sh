f71f3e13a7da4a5e9c27e718867f2f86ffd33dc2
Cori Haws <corihaws@radio-noise.net>
2018-09-23 00:45:01 +0100

    Run Pi-FM-RDS

--- scripts/pisstv.sh
+++ scripts/pisstv.sh
@@ -10,7 +10,7 @@ while true; do
   convert sstv.jpg -fill white -stroke black -pointsize 16 -annotate +50+50 'G3KMI de M0HWY\nQTH: University of Southampton\nWelcome!' sstv-out.jpg
   echo converting
   date
-  PYTHONPATH=/home/pi/.local/lib/python2.7/site-packages/pysstv python -m pysstv --mode Robot36 sstv-out.jpg sstv-out-tmp.wav
+  PYTHONPATH=HOMEDIR/.local/lib/python2.7/site-packages/pysstv python -m pysstv --mode Robot36 sstv-out.jpg sstv-out-tmp.wav
   touch lock
   echo moving
   mv sstv-out-tmp.wav sstv-out.wav
@@ -22,6 +22,7 @@ while true; do
   while [ -f lock ]; do
     true
   done
+  ./pi_fm_rds -freq 145.200 -audio HOMEDIR/sstv-out.wav -ps G3KMI -rt "G3KMI SSTV"
   ./pifm sstv-out.wav 145.200 48000
   echo repeating
   date
