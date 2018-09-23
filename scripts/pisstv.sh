991cddeaa948c0f7895cf4cf4fbfe360dfd8232d
Cori Haws <corihaws@radio-noise.net>
2018-09-23 20:55:41 +0100

    Correct paths

--- scripts/pisstv.sh
+++ scripts/pisstv.sh
@@ -7,7 +7,7 @@ while true; do
   raspistill --width 320 --height 240 -o sstv.jpg
   echo superimposing
   date
-  convert sstv.jpg -fill white -stroke black -pointsize 16 -annotate +50+50 'G3KMI de M0HWY\nQTH: University of Southampton\nWelcome!' sstv-out.jpg
+  convert sstv.jpg -fill white -stroke black -pointsize 16 -annotate +50+50 'G3KMI de M0YCH\nQTH: University of Southampton\nWelcome!' sstv-out.jpg
   echo converting
   date
   PYTHONPATH=HOMEDIR/.local/lib/python2.7/site-packages/pysstv python -m pysstv --mode Robot36 sstv-out.jpg sstv-out-tmp.wav
@@ -22,7 +22,7 @@ while true; do
   while [ -f lock ]; do
     true
   done
-  cat HOMEDIR/sstv-out.wav | ./pi_fm_rds -freq 145.200 -audio - -ps G3KMI -rt "G3KMI SSTV"
+  cat HOMEDIR/sstv-out.wav | sudo ./PiFmRds/src/pi_fm_rds -freq 145.200 -audio - -ps G3KMI -rt "G3KMI SSTV"
   ./pifm sstv-out.wav 145.200 48000
   echo repeating
   date
